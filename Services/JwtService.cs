5d9a10e56a3eabbd82182e909c31e20f136f4c49
dalzlad <dalz.sena@gmail.com>
2025-11-28 08:59:17 -0500

    Jwt parte 2

---
+++ Services/JwtService.cs
@@ -0,0 +1,47 @@
+﻿using ApiEmpresa.Models;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace ApiEmpresa.Services
+{
+    public class JwtService
+    {
+        public readonly IConfiguration _config;
+
+        public JwtService(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public string Generate(User user)
+        {
+            //La clave secreta para firmar el token
+            var key = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            //Claims: Información del usario que irá dentro los tokens
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.Name, user.UserName),
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(ClaimTypes.Role, user.Role),
+            };
+
+            //Crear el token
+            var token = new JwtSecurityToken(
+                issuer: _config["Jwt:Issuer"],
+                audience: _config["Jwt:Audience"],
+                claims: claims,
+                expires: DateTime.UtcNow.AddHours(2),
+                signingCredentials: creds
+                );
+            {
+                return new JwtSecurityTokenHandler().WriteToken(token);
+            };
+        }
+    }
+}
--- Services/PasswordService.cs
+++ Services/PasswordService.cs
@@ -5,11 +5,19 @@ namespace ApiEmpresa.Services
 {
     public class PasswordService
     {
+        // Convierte una contraseña a HASH usando SHA256
         public string Hash(string password)
         {
             var sha = SHA256.Create();
             var bytes = sha.ComputeHash(Encoding.UTF8.GetBytes(password));
             return Convert.ToBase64String(bytes);
         }
+
+        // Verifica si una contraseña es igual al hash guardado
+        public bool Verify(string password, string hash)
+        {
+            return Hash(password) == hash;
+        }
+
     }
 }
